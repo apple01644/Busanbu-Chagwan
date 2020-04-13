d57b7b53393bc8b3b403e413aff70a6f770310c5
yun_j_sang(윤재상) <apple01644@gmail.com>
2020-04-13 21:46:31 +0900

    Update band.py

--- band.py
+++ band.py
@@ -8,7 +8,6 @@ class Band:
         'band': 'https://openapi.band.us/v2.1/bands',
         'posts': 'https://openapi.band.us/v2/band/posts'
     }
-    last_fresh_time = 0
 
     def __init__(self):
         self.key = json.load(open('band_api.json', 'r'))
@@ -16,12 +15,13 @@ class Band:
         self.param_for_get_post = {'access_token': self.key['access_token'],
                                    'band_key': 'AADe-67DagLFl4xgYFYmMm0r',
                                    'locale': 'ko_KR'}
+        self.last_refresh_time = 0
         self.get_post()
 
     def get_post(self):
         posts = json.loads(req.get(self.urls['posts'], params=self.param_for_get_post).text)
-        result = [item for item in posts['result_data']['items'] if item['created_at'] >= self.last_fresh_time]
-        self.last_refresh_time = max([item['created_at'] for item in result])
+        result = [item for item in posts['result_data']['items'] if item['created_at'] > self.last_refresh_time]
+        self.last_refresh_time = max([item['created_at'] for item in result] + [self.last_refresh_time])
         return result
 
 
