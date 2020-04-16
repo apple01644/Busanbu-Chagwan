35bd8d948a747a0f0e2b169c64636e0298598deb
윤재상 <banana2692@daum.net>
2020-04-17 08:31:06 +0900

    Fix Early class

--- scheduler.py
+++ scheduler.py
@@ -31,37 +31,37 @@ async def class_loop(channel):
         last_run_date = datetime.datetime.now().date()
         last_run_time = now
 
-    if last_run_time < datetime.time(hour=8, minute=20) < now:
+    if last_run_time < datetime.time(hour=8, minute=20) <= now:
         last_run_time = now
         title = '%02d월 %02d일 알람방' % (last_run_date.month, last_run_date.day)
-        desc += '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n'
+        desc = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n'
         for k in range(3):
             desc += f'{k + 1}반 알림방: ' + spreadsheet.bookmarks[f'{k + 1}']['알림방'] + '\n'
+    else:
+        for the_class in classes:
+            if last_run_time < the_class['begin'] <= now:
+                title = f'{the_class["index"]}교시 시작'
+                text = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n\n'
+                last_run_time = now
+                for k in range(3):
+                    asd = spreadsheet.run_command(f'ㄱ시간표 {k + 1}반')
+                    class_data = spreadsheet.preprocess_command_data(asd)[f'{the_class["index"]} 교시']
 
-    for the_class in classes:
-        if last_run_time < the_class['begin'] <= now:
-            title = f'{the_class["index"]}교시 시작'
-            text = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n\n'
-            last_run_time = now
-            for k in range(3):
-                asd = spreadsheet.run_command(f'ㄱ시간표 {k + 1}반')
-                class_data = spreadsheet.preprocess_command_data(asd)[f'{the_class["index"]} 교시']
-
-                text += f'> {k + 1}반 {class_data["class_name"]}\n'
-                if class_data["teacher_list"]:
-                    text += f'> {class_data["teacher_list"]}\n'
-                if class_data["objective"]:
-                    text += f'> {class_data["objective"]}\n'
-                if class_data["class_data"]["link"]:
-                    text += f'> {class_data["class_data"]["link"]}\n'
-                text += '\n'
-            desc = text
-            break
-        if last_run_time < the_class['end'] <= now:
-            title = f'{the_class["index"]}교시 끝'
-            desc = None
-            last_run_time = now
-            break
+                    text += f'> {k + 1}반 {class_data["class_name"]}\n'
+                    if class_data["teacher_list"]:
+                        text += f'> {class_data["teacher_list"]}\n'
+                    if class_data["objective"]:
+                        text += f'> {class_data["objective"]}\n'
+                    if class_data["class_data"]["link"]:
+                        text += f'> {class_data["class_data"]["link"]}\n'
+                    text += '\n'
+                desc = text
+                break
+            if last_run_time < the_class['end'] <= now:
+                title = f'{the_class["index"]}교시 끝'
+                desc = None
+                last_run_time = now
+                break
 
     if title:
         await channel.send(embed=discord.Embed(title=title, description=desc))
