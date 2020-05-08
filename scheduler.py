7ba7dc794cc96f4bfd06a4f0c7a0ffe485dac92e
윤재상 <banana2692@daum.net>
2020-05-08 09:36:24 +0900

    Fix Scheudler Infinite Loop

--- scheduler.py
+++ scheduler.py
@@ -36,78 +36,70 @@ class Scheduler:
         if date.weekday() in [5, 6]:
             return
 
-        loop = True
-        while loop:
-            loop = False
-            if self.last_run_time < datetime.time(hour=8, minute=20) <= now:
-                loop = True
-                title = '%02d월 %02d일 알람방' % (self.last_run_date.month, self.last_run_date.day)
-                desc = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n'
-                for k in [1, 2, 3]:
-                    desc += f'{k}반 알림방: ' + spreadsheet.bookmarks[f'{k}']['알림방']["link"] + '\n'
-                desc += '\n다음 시간'
-                for k in [1, 2, 3]:
-                    class_data = spreadsheet.run_command(f'ㄱ시간표 {k}반')['1교시']
-                    desc += f'> {k}반 {class_data["class_name"]}\n'
+        if self.last_run_time < datetime.time(hour=8, minute=20) <= now:
+            title = '%02d월 %02d일 알람방' % (self.last_run_date.month, self.last_run_date.day)
+            desc = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n'
+            for k in [1, 2, 3]:
+                desc += f'{k}반 알림방: ' + spreadsheet.bookmarks[f'{k}']['알림방']["link"] + '\n'
+            desc += '\n다음 시간'
+            for k in [1, 2, 3]:
+                class_data = spreadsheet.run_command(f'ㄱ시간표 {k}반')['1교시']
+                desc += f'> {k}반 {class_data["class_name"]}\n'
+                if class_data["teacher_list"]:
+                    desc += f'> {class_data["teacher_list"]}\n'
+                if not class_data['헤더']['is_template']:
+                    if class_data["objective"]:
+                        desc += f'> {class_data["objective"]}\n'
+                if class_data["class_data"]:
+                    if class_data["class_data"]["link"]:
+                        desc += f'> {class_data["class_data"]["link"]}\n'
+            desc += '\n@everyone'
+            await self.alarm_channel.send(embed=discord.Embed(title=title, description=desc))
+
+            result = spreadsheet.run_command('ㄱ시간표', ['3학년 1반'])
+            if result['status'] == 200:
+                data = spreadsheet.command_data_to_description(result)
+                await self.first_channel.send(embed=discord.Embed(
+                    title=data['title'],
+                    description=data['description'] + f'\n<@&{static.discord_info["1st class role id"]}>'))
+
+            result = spreadsheet.run_command('ㄱ시간표', ['3학년 2반'])
+            if result['status'] == 200:
+                data = spreadsheet.command_data_to_description(result)
+                await self.second_channel.send(embed=discord.Embed(
+                    title=data['title'],
+                    description=data['description'] + f'\n<@&{static.discord_info["2nd class role id"]}>'))
+
+            result = spreadsheet.run_command('ㄱ시간표', ['3학년 3반'])
+            if result['status'] == 200:
+                data = spreadsheet.command_data_to_description(result)
+                await self.third_channel.send(embed=discord.Embed(
+                    title=data['title'],
+                    description=data['description'] + f'\n<@&{static.discord_info["3rd class role id"]}>'))
+
+        for the_class in config.schedule_info:
+            if self.last_run_time < the_class['begin'] <= now:
+                desc = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n\n'
+                for k in range(3):
+                    raw_data = spreadsheet.run_command(f'ㄱ시간표 {k + 1}반')
+                    class_data = spreadsheet.preprocess_command_data(raw_data)[f'{the_class["index"]} 교시']
+
+                    desc += f'> {k + 1}반 {class_data["class_name"]}\n'
                     if class_data["teacher_list"]:
                         desc += f'> {class_data["teacher_list"]}\n'
-                    if not class_data['헤더']['is_template']:
+                    if not raw_data['헤더']['is_template']:
                         if class_data["objective"]:
                             desc += f'> {class_data["objective"]}\n'
                     if class_data["class_data"]:
                         if class_data["class_data"]["link"]:
                             desc += f'> {class_data["class_data"]["link"]}\n'
-                desc += '\n@everyone'
-                await self.alarm_channel.send(embed=discord.Embed(title=title, description=desc))
-
-                result = spreadsheet.run_command('ㄱ시간표', ['3학년 1반'])
-                if result['status'] == 200:
-                    data = spreadsheet.command_data_to_description(result)
-                    await self.first_channel.send(embed=discord.Embed(
-                        title=data['title'],
-                        description=data['description'] + f'\n<@&{static.discord_info["1st class role id"]}>'))
-
-                result = spreadsheet.run_command('ㄱ시간표', ['3학년 2반'])
-                if result['status'] == 200:
-                    data = spreadsheet.command_data_to_description(result)
-                    await self.second_channel.send(embed=discord.Embed(
-                        title=data['title'],
-                        description=data['description'] + f'\n<@&{static.discord_info["2nd class role id"]}>'))
-
-                result = spreadsheet.run_command('ㄱ시간표', ['3학년 3반'])
-                if result['status'] == 200:
-                    data = spreadsheet.command_data_to_description(result)
-                    await self.third_channel.send(embed=discord.Embed(
-                        title=data['title'],
-                        description=data['description'] + f'\n<@&{static.discord_info["3rd class role id"]}>'))
-
-            for the_class in config.schedule_info:
-                if self.last_run_time < the_class['begin'] <= now:
-                    loop = True
-                    desc = '바로가기: https://classroom.google.com/u/1/a/not-turned-in/all\n\n'
-                    for k in range(3):
-                        raw_data = spreadsheet.run_command(f'ㄱ시간표 {k + 1}반')
-                        class_data = spreadsheet.preprocess_command_data(raw_data)[f'{the_class["index"]} 교시']
-
-                        desc += f'> {k + 1}반 {class_data["class_name"]}\n'
-                        if class_data["teacher_list"]:
-                            desc += f'> {class_data["teacher_list"]}\n'
-                        if not raw_data['헤더']['is_template']:
-                            if class_data["objective"]:
-                                desc += f'> {class_data["objective"]}\n'
-                        if class_data["class_data"]:
-                            if class_data["class_data"]["link"]:
-                                desc += f'> {class_data["class_data"]["link"]}\n'
-                        desc += '\n'
-                    await self.alarm_channel.send(
-                        embed=discord.Embed(
-                            title=f'{the_class["index"]}교시 시작',
-                            description=desc))
-                    break
-                elif self.last_run_time < the_class['end'] <= now:
-                    loop = True
-                    await self.alarm_channel.send(embed=discord.Embed(title=f'{the_class["index"]}교시 끝\n@everyone'))
-                    break
+                    desc += '\n'
+                await self.alarm_channel.send(
+                    embed=discord.Embed(
+                        title=f'{the_class["index"]}교시 시작',
+                        description=desc))
+            elif self.last_run_time < the_class['end'] <= now:
+                await self.alarm_channel.send(embed=discord.Embed(title=f'{the_class["index"]}교시 끝\n@everyone'))
         self.last_run_time = now
 
 
